dec7b4c6af22578374568d2d0342886d1262fc14
Pete Deffendol <pete@deffendol.org>
2020-12-29 11:48:08 -0700

    Add an endpoint that generates an exception to show error logging

---
+++ LoggingTraining.Api/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingTraining.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ErrorController : ControllerBase
+    {
+        public ErrorController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            throw new Exception("Manually generated exception");
+        }
+    }
+}
